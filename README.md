702ef45e13c583a0ea67ae6a609157c9923dc5b5
Carlos Maeso <carlos.maesogonzalez@gmail.com>
2015-07-05 14:07:40 +0200

    random version 3

---
+++ README.md
@@ -0,0 +1,4 @@
+# mi_random
+Comienzo con github
+Este proyecto es una copia del proyecto https://github.com/jquemada/random del profesor, al que se le ha añadido un archivo
+README.md y un archivo LICENSE.md
--- random.js
+++ random.js
@@ -1,3 +1,9 @@
+// ESTE FICHERO ES EL FICHERO RANDOM.JS DEL PROYECTO
+// https://github.com/jquemada/random DEL PROFESOR
+// NO VOY A MODIFICARLO, VOY A AÑADIR UN FICHERO README.md
+// Y UN FICHERO LICENSE.md TAL Y COMO INDICA EL EJERCICIO
+// TRAS ESTO, LOS SUBIRÉ AL PROYECTO MI_RANDOM DE GITHUB
+
 // Math.random() devuelve número aleatorio entre 0 y 1.
 var numero = Math.random();
 
